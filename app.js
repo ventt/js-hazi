25b32e29ac2f4b4fa62fa27847a21aa78d1c30be
ventt <adrian@szell.dev>
2022-03-10 17:28:00 +0100

    cardetails

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-
+var carDetailsRouter = require('./routes/cardetails')
 
 var app = express();
 
@@ -19,9 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routers
 app.use('/', indexRouter);
-
+app.use('/cardetails', carDetailsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
