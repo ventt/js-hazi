f9584d5c47774e4a33d0fe93acdcbad07af37f79
ventt <adrian@szell.dev>
2022-03-11 14:12:15 +0100

    cardetails done

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var readChunk = require('read-chunk');
+var imageType = require('image-type');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
